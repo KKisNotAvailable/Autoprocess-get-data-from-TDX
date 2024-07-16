ba70c2e129663a1fab4cad7897a129ebaea08cf9
KYK <ken19505@gmail.com>
2024-07-16 14:21:47 +0800

    finished multi point version, but face 2 tech issue. first is api call time per minute, second is no data for too closed to need public transportation village pairs.

--- TDX_retriever.py
+++ TDX_retriever.py
@@ -3,19 +3,45 @@ import json
 import requests
 from datetime import date, timedelta
 
+class BadResponse(Exception):
+    '''Bad response when retrieving'''
+    def __init__(self, message) -> None:
+        self.message = message
+        super(BadResponse, self).__init__(message)
+
 
 class TDX_retriever():
     def __init__(
             self, 
             app_id, app_key,
+            add_villcode: bool = False,
             auth_url="https://tdx.transportdata.tw/auth/realms/TDXConnect/protocol/openid-connect/token",
             url="https://tdx.transportdata.tw/api/maas/routing?"
         ):
         self.__app_id = app_id
         self.__app_key = app_key
-        self._auth_response = None
+        self.__add_villcode = add_villcode
         self.auth_url = auth_url
         self.url = url
+        self._auth_response = None
+        self.__cur_i = -1
+        self.__cur_j = -1
+        self.__log = ""
+
+        tmp_cols = []
+        if self.__add_villcode:
+            tmp_cols.extend(['A_villcode', 'B_villcode'])
+        tmp_cols.extend([
+            'A_lat', 'A_lon', 'B_lat', 'B_lon',
+            'AB_travel_time', 'AB_transfer_cnt', 'AB_route',
+            'BA_travel_time', 'BA_transfer_cnt', 'BA_route'
+        ])
+
+        self.__data = pd.DataFrame(columns=tmp_cols)
+
+    def get_log(self):
+        with open("log.txt", "w") as txt_file:
+            txt_file.write(self.__log)
 
     def _get_auth_header(self) -> dict:
         content_type = 'application/x-www-form-urlencoded'
@@ -127,7 +153,7 @@ class TDX_retriever():
 
         return requests.get(cur_url, headers=self._get_data_header())
     
-    def get_transport_result(self, coord_from: list, coord_to: list):
+    def get_transport_result(self, coord_from: list, coord_to: list) -> list:
         '''
         Will return back and forth results from the given two coords.
 
@@ -142,40 +168,104 @@ class TDX_retriever():
         # print(resp1)
         # print(resp1.text)
 
-        cur_row = [coord_from, coord_to]
+        cur_row = [*coord_from, *coord_to]
 
         for resp in resps:
-            df = pd.DataFrame(resp.json()['data']['routes'][0])
-            df['transport_mode'] = [d['transport']['mode'] for d in df['sections']]
-            df['mode_duration'] = [d['travelSummary']['duration'] for d in df['sections']]
+            self.__log += (
+                f"({self.__cur_i}, {self.__cur_j})" +
+                str(resp) + " " +
+                resp.text + "\n"
+            )
+            msg = (
+                f"Get {str(resp)}: {resp.json()['result']}"
+                f" when retrieving from (i, j)=({self.__cur_i}, {self.__cur_j})"
+            )
+            if "200" not in str(resp):
+                msg = "Bad Response! " + msg
+                raise BadResponse(msg)
+            
+            try:
+                df = pd.DataFrame(resp.json()['data']['routes'][0])
+                df['transport_mode'] = [d['transport']['mode'] for d in df['sections']]
+                df['mode_duration'] = [d['travelSummary']['duration'] for d in df['sections']]
 
-            cur_row.extend([
-                df['travel_time'].iloc[0], df['transfers'].iloc[0],
-                [[m, t] for m, t in zip(df['transport_mode'], df['mode_duration'])]
-            ])
+                cur_row.extend([
+                    df['travel_time'].iloc[0], df['transfers'].iloc[0],
+                    [[m, t] for m, t in zip(df['transport_mode'], df['mode_duration'])]
+                ])
+            except:
+                self.get_log()
+                raise BadResponse(msg)
                 
         return cur_row
 
+    def get_pairwise(self, coord_list: pd.DataFrame) -> pd.DataFrame:
+        # TODO: check if there's VILLCODE in the list before
+        # actually adding that into data.
+        n = coord_list.shape[0]
+        for i in range(n):
+            A_villcode = coord_list['VILLCODE'].iloc[i]
+            A_coord = [coord_list['lat'].iloc[i], coord_list['lon'].iloc[i]]
+            
+            self.__cur_i = i
+            for j in range(i+1, n):
+                B_villcode = coord_list['VILLCODE'].iloc[j]
+                B_coord = [coord_list['lat'].iloc[j], coord_list['lon'].iloc[j]]
+                self.__cur_j = j
 
-def main():
-    app_id = 'ken19505-7e99114f-1228-4355'
-    app_key = '15e3a19e-bf8d-44e8-9376-011e26de5133'
+                new_row = []
+                if self.__add_villcode:
+                    # TODO: change the list to be extended
+                    new_row.extend([A_villcode, B_villcode])
+                new_row.extend(self.get_transport_result(A_coord, B_coord))
+
+                self.__data.loc[len(self.__data)] = new_row
+
+        return self.__data
+
+
+def test_single(TDX: TDX_retriever):
     c1 = [24.9788580602204,121.55598430669878]
     c2 = [24.981549180333282,121.56397728822249]
 
+# 63000080031,121.55598430669878,24.9788580602204
+# 63000080032,121.56397728822249,24.981549180333282
+# 63000080041,121.55628472119042,24.98197093297431
+
     cols = [
-        'A_coord', 'B_coord', 
+        'A_lat', 'A_lon', 'B_lat', 'B_lon',
         'AB_travel_time', 'AB_transfer_cnt', 'AB_route',
         'BA_travel_time', 'BA_transfer_cnt', 'BA_route'
     ]
     df = pd.DataFrame(columns=cols)
-
-
-    TDX = TDX_retriever(app_id, app_key)
-
     df.loc[len(df)] = TDX.get_transport_result(c1, c2)
 
     # print(df)
+    # df.to_csv('single_pt_demo.csv', index=False)
+
+def main():
+    app_id = 'ken19505-7e99114f-1228-4355'
+    app_key = '15e3a19e-bf8d-44e8-9376-011e26de5133'
+
+    TDX = TDX_retriever(app_id, app_key)
+
+    # ===================================================
+    # Get single point DEMO: using get_transport_result()
+    # ===================================================
+    test_single(TDX)
+
+    # ===================================================
+    # Get pairwise from CSV
+    # ===================================================
+    # path = ".\\JJinTP_data_TW\\Routing\\"
+    # filename = "village_centroid_TP.csv"
+    # centroids = pd.read_csv(path+filename)
+
+    # # shorter for testing
+    # centroids = centroids.iloc[:3]
+
+    # df = TDX.get_pairwise(centroids)
+    # df.to_csv('multi_pt_demo.csv', index=False)
 
 
 if __name__ == "__main__":
