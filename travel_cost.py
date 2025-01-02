9ac8d33bf6314a735df56dc8a4520a46d1686a29
KYK <ken19505@gmail.com>
2025-01-02 15:19:31 +0800

    now travel cost works

--- travel_cost.py
+++ travel_cost.py
@@ -1,16 +1,30 @@
 import pandas as pd
 import numpy as np
-from scipy.special import factorial
+from scipy.special import factorial, gammaln
 from scipy.optimize import minimize
 from tqdm import tqdm
 
+DATA_PATH = "JJinTP_data_TW/"
 
-def travel_cost_single_year(
-        params: list,
-        public_travel_mat: np.ndarray,
-        private_travel_mat: np.ndarray,
-        public_transport_cnt_mat: np.ndarray,
-        private_transport_cnt_mat: np.ndarray
+def travel_cost(
+    params: list,
+    public_travel_mat: np.ndarray,
+    private_travel_mat: np.ndarray
+):
+    g_pub, g_pri, d_pub, d_pri = params
+    v = 1  # constant in current version
+
+    tmp_pub = np.exp((-1) * (d_pub * public_travel_mat - g_pub) / v)
+    tmp_pri = np.exp((-1) * (d_pri * private_travel_mat - g_pri) / v)
+
+    return -v * np.log(tmp_pub + tmp_pri)
+
+def travle_log_likelihood(
+    params: list,
+    public_travel_mat: np.ndarray,
+    private_travel_mat: np.ndarray,
+    public_transport_cnt_mat: np.ndarray,
+    private_transport_cnt_mat: np.ndarray
 ):
     '''
     All the matrix are np 2d array, rather than pd.DataFrame.
@@ -37,9 +51,10 @@ def travel_cost_single_year(
     # apply later to the result matrix to discard the no survey data location pairs
     have_transport_cnt = tot_transport_cnt_mat > 0
 
-    ln_Lt_mat = np.log(factorial(tot_transport_cnt_mat)) \
-        - np.log(factorial(private_transport_cnt_mat)) \
-        - np.log(factorial(public_transport_cnt_mat)) \
+    # since scipy's factorial cannot work with 0s, use gammaln instead
+    ln_Lt_mat = gammaln(tot_transport_cnt_mat + 1) \
+        - gammaln(private_transport_cnt_mat + 1) \
+        - gammaln(public_transport_cnt_mat + 1) \
         + private_transport_cnt_mat * np.log(prob_pri_mat) \
         + public_transport_cnt_mat * np.log(prob_pub_mat)
     
@@ -48,33 +63,19 @@ def travel_cost_single_year(
 
     return -ln_L  # for optimization, using function min but want to find max
 
-def ttl_travle_cost(
-        params: list,
-        public_travel_mat: np.ndarray,
-        private_travel_mat: np.ndarray
-):
-    '''
-    '''
-    # TODO: but do we have survey of travel pairs for each year? or just combined?
 
-
-def find_param(initial_params):
+def find_param(initial_params, public_travel_mat, private_travel_mat):
     g_pub, g_pri = initial_params["gamma_public"], initial_params["gamma_private"]
     d_pub, d_pri = initial_params["delta_public"], initial_params["delta_private"]
     # v = initial_params["v"]
 
     init_param_set = [g_pub, g_pri, d_pub, d_pri]
 
-    # 1. get the matrix from travel time data
-    # public_travel_mat = to_mat()
-    # private_travel_mat = to_mat()
+    # 1. using all year combined survey:
+    public_transport_cnt_mat = pd.read_csv(f"{DATA_PATH}public_mode_cnt.csv").values
+    private_transport_cnt_mat = pd.read_csv(f"{DATA_PATH}private_mode_cnt.csv").values
 
-    # 2. not sure currently we are using survey for different years or just combined
-    #    given using combined:
-    # public_transport_cnt_mat = to_mat()
-    # private_transport_cnt_mat = to_mat()
-
-    # 3. start optimizing
+    # 2. start optimizing
     # constraints example
     constraints = [
         {'type': 'ineq', 'fun': lambda params: params[0] + params[1] + params[2] - 1},  # x + y + z >= 1
@@ -85,16 +86,17 @@ def find_param(initial_params):
     ]
 
     res = minimize(
-        travel_cost_single_year,
+        travle_log_likelihood,
         init_param_set,
-        # args=(public_travel_mat, private_travel_mat, public_transport_cnt_mat, private_transport_cnt_mat),
+        args=(public_travel_mat, private_travel_mat, public_transport_cnt_mat, private_transport_cnt_mat),
         # constraints=constraints
         # can also set bounds to the params
     )
 
-    # Extract optimal parameters
-    optimal_params = res.x
-    optimal_cost = res.fun
+    print(f"Optimization is success: {res.success}")
+    print(f"Nuber of iterations {res.nit}")
+
+    return res.x  # optimal parameters
 
 
 def main():
@@ -108,6 +110,24 @@ def main():
         "v": 1
     }
 
+    public_travel_mat = pd.read_csv(f"{DATA_PATH}public_town_travel_mat.csv").values
+    public_travel_mat = public_travel_mat / 60
+    private_travel_mat = pd.read_csv(f"{DATA_PATH}private_town_travel_mat.csv").values
+    private_travel_mat = private_travel_mat / 60
+
+    optimal_params = find_param(
+        params,
+        public_travel_mat=public_travel_mat,
+        private_travel_mat=private_travel_mat
+    )
+
+    tc = travel_cost(
+        optimal_params,
+        public_travel_mat=public_travel_mat,
+        private_travel_mat=private_travel_mat
+    )
+
+    # print(tc)
 
 
 if __name__ == "__main__":
