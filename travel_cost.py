9f1832a1a532bf6b0272bfd1f772b0215cbe9577
KYK <ken19505@gmail.com>
2025-01-21 10:32:13 +0800

    now completes the bilateral commuting cost for both towns and villages

--- travel_cost.py
+++ travel_cost.py
@@ -1,10 +1,21 @@
 import pandas as pd
 import numpy as np
 from scipy.special import factorial, gammaln
-from scipy.optimize import minimize
+from scipy.optimize import minimize, basinhopping
+import math
+import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 DATA_PATH = "JJinTP_data_TW/"
+
+
+class LargerStep:
+    def __init__(self, stepsize=1.0):  # Adjust stepsize as needed
+        self.stepsize = stepsize
+
+    def __call__(self, x):
+        return x + np.random.uniform(-self.stepsize, self.stepsize, size=len(x))
+
 
 def travel_cost(
     params: list,
@@ -19,7 +30,7 @@ def travel_cost(
 
     return -v * np.log(tmp_pub + tmp_pri)
 
-def travle_log_likelihood(
+def travel_log_likelihood(
     params: list,
     public_travel_mat: np.ndarray,
     private_travel_mat: np.ndarray,
@@ -64,7 +75,7 @@ def travle_log_likelihood(
     return -ln_L  # for optimization, using function min but want to find max
 
 
-def find_param(initial_params, public_travel_mat, private_travel_mat):
+def find_param(initial_params, public_travel_mat, private_travel_mat, show_stats=True):
     g_pub, g_pri = initial_params["gamma_public"], initial_params["gamma_private"]
     d_pub, d_pri = initial_params["delta_public"], initial_params["delta_private"]
     # v = initial_params["v"]
@@ -85,41 +96,164 @@ def find_param(initial_params, public_travel_mat, private_travel_mat):
         {'type': 'ineq', 'fun': lambda params: params[0]}                               # x >= 0
     ]
 
+    # parameter bounds
+    bounds = [
+        (None, 0),  # gamma_public <= 0
+        (None, 0),  # gamma_private <= 0
+        (0, None),  # delta_public <= 0
+        (0, None),  # delta_public <= 0
+    ]
+
     res = minimize(
-        travle_log_likelihood,
+        travel_log_likelihood,
         init_param_set,
         args=(public_travel_mat, private_travel_mat, public_transport_cnt_mat, private_transport_cnt_mat),
+        method='L-BFGS-B',
+        bounds=bounds
         # constraints=constraints
-        # can also set bounds to the params
     )
 
-    print(f"Optimization is success: {res.success}")
-    print(f"Nuber of iterations {res.nit}")
+    # res = basinhopping(
+    #     func=travel_log_likelihood,
+    #     x0=init_param_set,
+    #     minimizer_kwargs={
+    #         "args": (public_travel_mat, private_travel_mat, public_transport_cnt_mat, private_transport_cnt_mat),
+    #         "method": 'L-BFGS-B', 
+    #         "bounds": bounds
+    #     },
+    #     niter=200,  # Number of basin-hopping iterations
+    #     # take_step=LargerStep(stepsize=0.5),  # Increase step size
+    #     seed=527  # For reproducibility
+    # )
 
-    return res.x  # optimal parameters
+    if show_stats:
+        print(f"Optimization is success: {res.success}")
+        print(f"Number of iterations {res.nit}")
+        print(f"Calculated likelihood {res.fun}")  # want this number as small as possible
+        print(f"Optimal parameters: {res.x}")
+
+    # The following is just to make sure res.fun is giving the correct result.
+    # lkh = travel_log_likelihood(
+    #     params=res.x,
+    #     public_travel_mat=public_travel_mat,
+    #     private_travel_mat=private_travel_mat,
+    #     public_transport_cnt_mat=public_transport_cnt_mat,
+    #     private_transport_cnt_mat=private_transport_cnt_mat
+    # )
+    # print(lkh)
+
+    return res.x, res.fun  # optimal parameters and corresponding likelihood
+
+
+def manual_minimize(
+    init_val: float, bounds: list, 
+    public_travel_mat, private_travel_mat,
+    show_plot=False, **step_settings
+):
+    '''
+    Since the minimization function seemed trapped in the local minimum, this
+    function manually test different initial values and check where the global
+    minimum objective function value locates.
+
+    Parameters
+    ----------
+    init_val: float.
+        This value must lies in the bounds, since we are finding the list
+        of values to be tested within the bound based on this value and
+        the step settings.
+    bounds: list.
+        It is expected to receive two values in the list, like this
+        [lower_bound, upper_bound].
+    show_plot: bool.
+        To stat if need to show the likelihood series plot (to check if the
+        sequence is converging).
+    step_settings
+        Currently consist of step_size and step_method. Will use the stated
+        settings to find test values within the range, starting from init_val.
+    '''
+    step_size = step_settings['step_size']
+    mthd = step_settings['step_method']
+
+    if mthd == '*':
+        l_cnt = abs(int(math.log(abs(bounds[0]), step_size)))
+        u_cnt = abs(int(math.log(abs(bounds[1]), step_size)))
+
+        test_vals_l = [init_val / step_size ** i for i in range(1, l_cnt+1)]
+        test_vals_u = [init_val * step_size ** i for i in range(1, u_cnt+1)]
+    elif mthd == '+':
+        l_cnt = int(abs(bounds[0] - init_val) / step_size)
+        u_cnt = int(abs(bounds[1] - init_val) / step_size)
+
+        test_vals_l = [init_val - step_size * i for i in range(1, l_cnt+1)]
+        test_vals_u = [init_val + step_size * i for i in range(1, u_cnt+1)]
+
+    test_vals = test_vals_l[::-1] + [init_val] + test_vals_u
+
+    # print(test_vals)
+
+    record = []
+
+    for v in test_vals:
+        params = {
+            "gamma_public": v,  # fixed cost
+            "gamma_private": v, 
+            "delta_public": 0.5,  # marginal cost (of travel time)
+            "delta_private": 0.5, 
+            "v": 1  # kind of like the likelihood of substitution between modes, close to 1 means low substitution.
+        }
+
+        optimal_params, lkh = find_param(
+            params,
+            public_travel_mat=public_travel_mat,
+            private_travel_mat=private_travel_mat,
+            show_stats=False
+        )
+
+        record.append({'params': optimal_params, 'lkh': lkh})
+
+    srs = [(rec['lkh'] - 1639.58845) * 1000000 for rec in record]
+    srs = [val for val in srs if val < 3]
+
+    if show_plot:
+        plt.plot(srs, marker='o', markersize=3, linestyle='-', color='black', label='Series')
+        # plt.xticks(ticks=range(len(srs)), labels=test_vals, fontsize=8)
+        plt.ylim(2.4605, 2.4615)
+        plt.xticks(ticks=[0, len(srs)-1], labels=[min(test_vals), max(test_vals)], fontsize=8)
+        plt.grid(axis='y', linestyle='--', alpha=0.7)  # "True" for both direction
+        plt.show()
+
+    opt_param = min(record, key=lambda x: x['lkh'])
+
+    return opt_param
 
 
 def main():
-    print("Start getting travel cost.")
+    print("Start getting travel cost...")
 
-    params = {
-        "gamma_public": 1, 
-        "gamma_private": 3, 
-        "delta_public": 0.1, 
-        "delta_private": 0.3, 
-        "v": 1
-    }
+    public_travel_mat = pd.read_csv(f"{DATA_PATH}public_town_travel_mat.csv")
+    towns = public_travel_mat.columns
 
-    public_travel_mat = pd.read_csv(f"{DATA_PATH}public_town_travel_mat.csv").values
-    public_travel_mat = public_travel_mat / 60
+    public_travel_mat = public_travel_mat.values / 60
     private_travel_mat = pd.read_csv(f"{DATA_PATH}private_town_travel_mat.csv").values
     private_travel_mat = private_travel_mat / 60
 
-    optimal_params = find_param(
-        params,
+    # Manual search for 'global' minimum 
+    # (tested on the range of -99 ~ -1 ~ -0.01, and found the minimum is more
+    # likely lies in -1~-0.01, based on the plot of the likelihood values)
+    step_setting = {
+        "step_size": 0.01,  # other test: 1
+        "step_method": '+'
+    }
+
+    opt = manual_minimize(
+        init_val=-1, 
+        bounds=[-1, -0.01],  # other test: [-99, -1]
         public_travel_mat=public_travel_mat,
-        private_travel_mat=private_travel_mat
+        private_travel_mat=private_travel_mat,
+        **step_setting
     )
+    optimal_params = opt['params']
+
 
     public_vill_travel_mat = pd.read_csv(f"{DATA_PATH}public_vill_travel_mat.csv")
     vills = public_vill_travel_mat.columns
@@ -129,17 +263,25 @@ def main():
     private_vill_travel_mat = private_vill_travel_mat / 60
 
 
-    tc = travel_cost(
+    tc_town = travel_cost(
+        optimal_params,
+        public_travel_mat=public_travel_mat,
+        private_travel_mat=private_travel_mat
+    )
+
+    tc_town = pd.DataFrame(tc_town, columns=towns)
+
+    tc_vill = travel_cost(
         optimal_params,
         public_travel_mat=public_vill_travel_mat,
         private_travel_mat=private_vill_travel_mat
     )
 
-    tc = pd.DataFrame(tc, columns=vills)
+    tc_vill = pd.DataFrame(tc_vill, columns=vills)
 
-    print(tc)
+    print(tc_vill)
 
-    # tc.to_csv(DATA_PATH+"commuting_cost.csv", index=False)
+    # tc.to_csv(DATA_PATH+"towns_commuting_cost.csv", index=False)
 
 
 if __name__ == "__main__":
